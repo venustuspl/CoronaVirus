2523f45c09adb074818ba1856a400ec9560585ba
venustuspl <venustus.pl@gmail.com>
2020-03-09 21:52:09 +0100

    0.1

---
+++ src/main/java/pl/venustus/CoronaVirus/CoronaVirusApplication.java
@@ -0,0 +1,13 @@
+package pl.venustus.CoronaVirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaVirusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaVirusApplication.class, args);
+	}
+
+}
