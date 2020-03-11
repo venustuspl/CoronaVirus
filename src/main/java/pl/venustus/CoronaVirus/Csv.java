7ab46a6b8b80fb9432a013cf231b2d1415b1b3f3
venustuspl <venustus.pl@gmail.com>
2020-03-11 21:56:08 +0100

    0.3

--- src/main/java/pl/venustus/CoronaVirus/Csv.java
+++ src/main/java/pl/venustus/CoronaVirus/Csv.java
@@ -10,20 +10,26 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 
 @Service
 public class Csv {
 
-    public void downloadCsv() throws IOException {
-        InputStream input = new URL("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-10.xls").openStream();
+    public Iterable<CSVRecord> downloadCsv() throws IOException {
+        int month = LocalDate.now().getMonthValue();
+        int day = LocalDate.now().getDayOfMonth();
+        System.out.println(day);
+        InputStream input = new URL("https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-0" + month + "-" + day + ".xls").openStream();
 
         Reader reader = new InputStreamReader(input, StandardCharsets.UTF_8);
 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
 
-        for (CSVRecord entry : records) {
-            System.out.println(entry);
-
-        }
+//        for (CSVRecord entry : records) {
+//            System.out.println(entry.get(0) + " " + entry.get(1));
+//
+//        }
+//
+        return records;
     }
 }
