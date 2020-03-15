2f4b7d783bfbf49b3b0a8c0119a75319c913e478
venustuspl <venustus.pl@gmail.com>
2020-03-15 15:06:37 +0100

    1.6

--- src/main/resources/static/api.js
+++ src/main/resources/static/api.js
@@ -14,80 +14,18 @@ function append(parent, el) {
     return parent.appendChild(el); // Append the second parameter(element) to the first one
 }
 
+const ad = document.getElementById('alldata');
 
-const os = document.getElementById('savenewoffers');
-
-fetch("/api/savenewoffers")
+fetch("/alldata")
     .then((resp) => resp.json()) // Transform the data into json
     .then(function (data) {
-
-        // Map through the results and for each run the code below
-        let li = createNode('li'), //  Create the elements we need
-            span = createNode('span');
-        li.innerHTML = `${data}`; // Make the HTML of our span to be the first and last name of our author
-        append(li, span);
-        append(os, li);
-
-    });
-
-const oc = document.getElementById('offerscount');
-
-fetch("api/online/offerscount")
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-
-        // Map through the results and for each run the code below
-        let li = createNode('li'), //  Create the elements we need
-            span = createNode('span');
-        li.innerHTML = `${data}`; // Make the HTML of our span to be the first and last name of our author
-        append(li, span);
-        append(oc, li);
-
-    });
-
-
-const oo = document.getElementById('offersonline');
-
-fetch("api/online/offers")
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-        let offers = data; // Get the results
-        return offers.map(function (offer) { // Map through the results and for each run the code below
+        let countries = data; // Get the results
+        return countries.map(function (country) { // Map through the results and for each run the code below
             let li = createNode('li'), //  Create the elements we need
                 span = createNode('span');
-            li.innerHTML = `${offer} `; // Make the HTML of our span to be the first and last name of our author
+            li.innerHTML = `${country} `; // Make the HTML of our span to be the first and last name of our author
             append(li, span);
-            append(oo, li);
+            append(ad, li);
         })
     });
 
-
-const ul = document.getElementById('offers');
-
-fetch("api/db/offers")
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-        let offers = data; // Get the results
-        return offers.map(function (offer) { // Map through the results and for each run the code below
-            let li = createNode('li'), //  Create the elements we need
-                span = createNode('span');
-            li.innerHTML = ` <a href="${offer.url}" target=”_blank”>  ${offer.name}  </a> Data zapisu do bazy: ${offer.dateTime}`; // Make the HTML of our span to be the first and last name of our author
-            append(li, span);
-            append(ul, li);
-        })
-    });
-
-const lo = document.getElementById('lastoffer');
-
-fetch("/api/db/last/offerdatetime")
-    .then((resp) => resp.json()) // Transform the data into json
-    .then(function (data) {
-
-        // Map through the results and for each run the code below
-        let li = createNode('li'), //  Create the elements we need
-            span = createNode('span');
-        li.innerHTML = `${data}`; // Make the HTML of our span to be the first and last name of our author
-        append(li, span);
-        append(lo, li);
-
-    });
