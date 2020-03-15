4e97a8cca13e4d29179f351526ee6b77643cefeb
venustuspl <venustus.pl@gmail.com>
2020-03-15 10:01:47 +0100

    1.1

--- src/main/java/pl/venustus/CoronaVirus/CsvController.java
+++ src/main/java/pl/venustus/CoronaVirus/CsvController.java
@@ -1,22 +1,24 @@
 package pl.venustus.CoronaVirus;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.util.Map;
 
-@Controller
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/v1")
 public class CsvController {
     @Autowired
     Csv csv;
 
-    @GetMapping("/showRecords")
-    public void showRecords() throws IOException {
-        Map<String, Double> map = csv.downloadCsv();
-        for (Map.Entry<String, Double> entry : map.entrySet()) {
-            System.out.println(entry.getKey() + " " + entry.getValue());
-        }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/alldata")
+    public Map<String, Double> showRecords() throws IOException {
+        return csv.downloadCsv();
     }
 }
